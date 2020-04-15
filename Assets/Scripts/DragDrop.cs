355fb88c67002f5fe3f24f9c68165835b75c44dd
Tydyus <tydyus@gmail.com>
2020-04-15 20:08:55 +0200

    initUnityProject
    Initialisation du Project unity

---
+++ Assets/Scripts/DragDrop.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+    private RectTransform rectTransform;
+
+    private void Awake(){
+        rectTransform = GetComponent<RectTransform>();
+    }
+    public void OnBeginDrag(PointerEventData evenData){
+        Debug.Log("You drag ???.");
+    }
+    public void OnDrag(PointerEventData evenData){
+        rectTransform.anchoredPosition += evenData.delta;
+    }
+    public void OnEndDrag(PointerEventData evenData){
+        Debug.Log("You drop ???.");
+    }
+    public void OnPointerDown(PointerEventData evenData) {
+        Debug.Log("You grab ???.");
+    }
+}
