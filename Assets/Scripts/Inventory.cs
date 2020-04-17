438317f04c290b411822e694bc5972ff19f91eae
Thibault Lecointe <thibault.lecointe@epitech.eu>
2020-04-18 01:22:08 +0200

    inventory

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class inventory
+{
+    private static int TotalSlots = 10;
+    private static int UsedSlots = 0;
+    private static List<string> items;
+
+    public bool additem(string item)
+    {
+        if (UsedSlots < TotalSlots)
+        {
+            items.Add(item);
+            return true;
+        }
+        return false;
+    }
+    public int getFreeSpace()
+    {
+        return (TotalSlots - UsedSlots);
+    }
+
+    public List<string> getItems()
+    {
+        return items;
+    }
+
+}
