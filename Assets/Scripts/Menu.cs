8b3981ad5943e7d974b70b883fe1d237f8389bb8
Thibault Lecointe <thibault.lecointe@epitech.eu>
2020-04-26 20:49:24 +0200

    Shortcut for menu (esc key)

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _Menu;
+    private bool FlipFlop;
+    void Start()
+    {
+        FlipFlop = false;
+    }
+    void Update()
+    {
+        if (Input.GetAxis("Menu") != 0 && FlipFlop == false)
+        {
+            FlipFlop = true;
+            _Menu.SetActive(!_Menu.activeSelf);
+        }
+        if (Input.GetAxis("Menu") == 0 && FlipFlop == true)
+        {
+            FlipFlop = false;
+        }
+    }
+}
