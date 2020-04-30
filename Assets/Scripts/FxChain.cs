92a64eafad2401d79dbbc7e83d8da0ca35522951
Tydyus <tydyus@gmail.com>
2020-04-30 11:45:22 +0200

    maj effect + add on prefab + add chainFx's script

---
+++ Assets/Scripts/FxChain.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FxChain : MonoBehaviour
+{
+    private Transform child;
+
+    private void Awake()
+    {
+        child = transform.GetChild(0);
+    }
+    public void PlayFX()
+    {
+        
+        if (child != null)
+        {
+            if (child.GetComponent<ParticleSystem>())
+                child.GetComponent<ParticleSystem>().Play();
+            if (child.GetComponent<FxChain>())
+                child.GetComponent<FxChain>().PlayFX();
+        }
+    }
+
+
+}
