6ab7895b618e7e2b48836a3e57e4a988552f01b5
Thibault Lecointe <thibault.lecointe@epitech.eu>
2020-05-03 01:52:19 +0200

    Highlight.cs

--- Assets/Scripts/Highlight.cs
+++ Assets/Scripts/Highlight.cs
@@ -5,19 +5,23 @@ using UnityEngine.UI;
 
 public class Highlight : MonoBehaviour
 {
-    public void hightlightIt(SpriteRenderer mySprite)
+    public void hightlightIt()
     {
+        var mySprite = transform.GetComponent<SpriteRenderer>();
         mySprite.color = new Color(mySprite.color.r,mySprite.color.g,mySprite.color.b,0.7f);
     }
-    public void UnhightlightIt(SpriteRenderer mySprite)
+    public void UnhightlightIt()
     {
+        var mySprite = transform.GetComponent<SpriteRenderer>();
         mySprite.color = new Color(mySprite.color.r,mySprite.color.g,mySprite.color.b,1f);
     }
 
-    public void hightlightimageIt(Image mySprite) {
+    public void hightlightimageIt() {
+        var mySprite = transform.GetComponent<Image>();
          mySprite.color = new Color(mySprite.color.r,mySprite.color.g,mySprite.color.b,0.7f);
     }
-    public void UnhightlightimageIt(Image mySprite) {
+    public void UnhightlightimageIt() {
+        var mySprite = transform.GetComponent<Image>();
         mySprite.color = new Color(mySprite.color.r,mySprite.color.g,mySprite.color.b,1f);
     }
 }
